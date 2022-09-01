702fe3ae951aa473508c9a8b183556007da021c4
Luis Medel <luis@luismedel.com>
2022-09-01 23:30:37 +0200

    Echo empty requests

--- 0-smoke-test/EchoServer.cs
+++ 0-smoke-test/EchoServer.cs
@@ -29,11 +29,8 @@ namespace SmokeTest
                 var data = await ReadData (client, ct);
                 Trace.WriteLine ($"> Received {data.Length} bytes from {client.Client.RemoteEndPoint}");
 
-                if (data.Length > 0)
-                {
-                    await SendData (client, data, ct);
-                    Trace.WriteLine ($"> Sent {data.Length} bytes to {client.Client.RemoteEndPoint}");
-                }
+                await SendData (client, data, ct);
+                Trace.WriteLine ($"> Sent {data.Length} bytes to {client.Client.RemoteEndPoint}");
 
                 Trace.WriteLine ($"> Closing connection to {client.Client.RemoteEndPoint}.");
                 client.Close ();
