09231e4ee6f4b8ec20ed5b01cfa9f67e9f5abf4e
Luis Medel <luis@medel.email>
2022-09-08 22:22:01 +0200

    Add template

---
+++ 99-template/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using CommandLine;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Protohackers
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option ('a', "addr", Required = false, Default = "0.0.0.0", HelpText = "Address")]
+            public string IpAddress { get; set; } = String.Empty;
+
+            [Option ('p', "port", Required = false, Default = 7777, HelpText = "Port")]
+            public int Port { get; set; }
+
+            [Option ("trace", Required = false, Default = true, HelpText = "Show trace")]
+            public bool ShowTrace { get; set; }
+        }
+
+        static void Main (string[] args)
+        {
+            Parser.Default
+                  .ParseArguments<Options> (args)
+                  .WithParsed<Options> (Run);
+        }
+
+        static void Run (Options o)
+        {
+            if (o.ShowTrace)
+                Logger.AddTraceListener (new ConsoleTraceListener ());
+
+            using (var cts = new CancellationTokenSource ())
+            {
+                var ct = cts.Token;
+
+                Logger.Info ($"Binding server to {o.IpAddress}:{o.Port}...");
+                Server server = new Server (o.IpAddress, o.Port);
+
+                var t = StartServer (server, ct);
+
+                Console.WriteLine ("Press [q] to quit the server...");
+
+                while (server.IsRunning)
+                {
+                    if (Console.Read () != 'q')
+                        break;
+                }
+
+                cts.Cancel ();
+                t.Wait ();
+            }
+        }
+
+        static Task StartServer (Server server, CancellationToken ct)
+        {
+            Task t = new Task (() => {
+                try
+                {
+                    server.Run (ct)
+                          .GetAwaiter ()
+                          .GetResult ();
+                }
+                catch (OperationCanceledException)
+                {
+                }
+                catch (Exception ex)
+                {
+                    Logger.Exception (ex);
+                    throw;
+                }
+                finally
+                {
+                    Logger.Info ($"Server closed.");
+                }
+            }, ct);
+
+            t.Start ();
+
+            while (!server.IsRunning)
+                Task.Delay (100);
+
+            return t;
+        }
+    }
+}
---
+++ 99-template/Server.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Protohackers
+{
+    public class Server
+    {
+        const int MAX_TASKS = 1024;
+        const int ALLOC_TASK_RETRIES = 5;
+
+        public bool IsRunning { get; private set; } = false;
+
+        public Server (string addr, int port)
+        {
+            _endpoint = new IPEndPoint (IPAddress.Parse (addr), port);
+        }
+
+        bool AddTask(Action action)
+        {
+            int retries = 0;
+
+            Task t = new Task (action);
+            while (true)
+            {
+                lock (_runningTasks)
+                {
+                    if (_runningTasks.Count == MAX_TASKS)
+                    {
+                        _runningTasks.RemoveAll (t => t.IsCompleted);
+                        if (_runningTasks.Count == MAX_TASKS)
+                        {
+                            Logger.Warn ($"Max tasks reached ({MAX_TASKS})");
+                            if (++retries < ALLOC_TASK_RETRIES)
+                                continue;
+
+                            return false;
+                        }
+                    }
+
+                    _runningTasks.Add (t);
+                    t.Start ();
+
+                    return true;
+                }
+            }
+        }
+
+        void HandleClient (TcpClient client, CancellationToken ct)
+        {
+            // Do the client handling here
+
+            if (client.Connected)
+            {
+                Logger.Debug ($"Closing connection to {client.Client.RemoteEndPoint}...");
+                client.Close ();
+            }
+        }
+
+        public async Task Run (CancellationToken ct)
+        {
+            if (this.IsRunning)
+                return;
+
+            this.IsRunning = true;
+
+            var listener = new TcpListener (_endpoint);
+            listener.Start ();
+
+            Logger.Info ($"Listening for connections in {_endpoint}...");
+
+            while (this.IsRunning && !ct.IsCancellationRequested)
+            {
+                try
+                {
+                    var client = await listener.AcceptTcpClientAsync (ct);
+                    if (client == null)
+                        continue;
+
+                    Logger.Debug ($"Accepted connection from {client.Client.RemoteEndPoint}");
+
+                    if (!AddTask (() => HandleClient (client, ct)))
+                    {
+                        Logger.Error ($"Stopping server. Reason: Can't allocate new tasks");
+                        break;
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Exception (ex);
+                }
+            }
+
+            await this.EndTasks ();
+
+            this.IsRunning = false;
+        }
+
+        async Task EndTasks()
+        {
+            var pending = _runningTasks.Where (t => !t.IsCompleted).ToArray ();
+            if (pending.Length == 0)
+                return;
+
+            Logger.Info ($"Awaiting {pending.Length} tasks to complete...");
+
+            await Task.WhenAll (pending);
+        }
+
+        public void Stop ()
+        {
+            this.IsRunning = false;
+        }
+
+        readonly IPEndPoint _endpoint;
+        readonly List<Task> _runningTasks = new List<Task> (MAX_TASKS);
+    }
+}
